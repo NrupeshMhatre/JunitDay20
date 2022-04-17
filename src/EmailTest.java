6876ac6fa10d7a82ec3f86bc56406630e2e4a703
NrupeshMhatre <100428751+NrupeshMhatre@users.noreply.github.com>
2022-04-17 13:30:31 +0530

    Valid Entry

---
+++ src/EmailTest.java
@@ -0,0 +1,19 @@
+package testpackage;
+
+import mainpackage.EmailValidator;
+
+public class EmailTest {
+	private static final String[] validEmailIds = new String[] { "abc@yahoo.com", "abc-100@yahoo.com",
+			"abc.100@yahoo.com", "abc111@yahoo.com", "abc-100@abc.net",
+			"abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+			"abc+100@gmail.com"};
+
+
+	public static void main(String args[]) {
+		EmailValidator emailValidator = new EmailValidator();
+		for (String temp : validEmailIds) {
+			boolean valid = emailValidator.validateEmail(temp);
+			System.out.println("Email ID " + temp + " is valid? " + valid);
+		}
+}
+}
