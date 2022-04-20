a6bc02d0710822cea3ffe02e1468ec2e5baec1ce
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-20 23:38:05 +0530

    Parameterised Test

--- src/main/java/mainpackage/EmailValidator.java
+++ src/main/java/mainpackage/EmailValidator.java
@@ -2,17 +2,12 @@ package mainpackage;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class EmailValidator {
-	private static final String EMAIL_REGEX = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-	private static Pattern pattern;
-	private Matcher matcher;
-
-	public EmailValidator() {
-		pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
-	}
-
-	
-	public boolean validateEmail(String email) {
-		matcher = pattern.matcher(email);
-		return matcher.matches();
+public boolean validateEmail(String testString) {
+		
+		String regex="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher match=pattern.matcher(testString);
+		boolean result=match.find();
+		return result;
 }
 }
--- src/test/java/testpackage/EmailTest.java
+++ src/test/java/testpackage/EmailTest.java
@@ -1,19 +1,67 @@
 package testpackage;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import mainpackage.EmailValidator;
-
+@RunWith(Parameterized.class)
 public class EmailTest {
-	private static final String[] validEmailIds = new String[] { "abc@yahoo.com", "abc-100@yahoo.com",
-			"abc.100@yahoo.com", "abc111@yahoo.com", "abc-100@abc.net",
-			"abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
-			"abc+100@gmail.com"};
+	
+	   
+	       private String emailTest;
+	       private boolean expectedResult;
+	       private EmailValidator validateEmail;
 
+	       public EmailTest(String emailTest, boolean expectedResult) {
+	           this.emailTest = emailTest;
+	           this.expectedResult = expectedResult;
+	       }
 
-	public static void main(String args[]) {
-		EmailValidator emailValidator = new EmailValidator();
-		for (String temp : validEmailIds) {
-			boolean valid = emailValidator.validateEmail(temp);
-			System.out.println("Email ID " + temp + " is valid? " + valid);
-		}
-}
+	       @Before
+	       public void initialize() {
+	           validateEmail = new EmailValidator();
+	       }
+
+	       @Parameterized.Parameters
+	       public static Collection data() {
+	           return Arrays.asList(new Object[][] {
+	                   {"abc@yahoo.com", true},
+	                   {"abc-100@yahoo.com", true},
+	                   {"abc.100@yahoo.com", true},
+	                   {"abc111@abc.com", true},
+	                   {"abc-100@abc.net", true},
+	                   {"abc.100@abc.com.au", true},
+	                   {"abc@1.com", true},
+	                   {"abc@gmail.com.com", true},
+	                   {"abc+100@gmail.com", true},
+	                   {"abc", false},
+	                   {"abc@.com.my", false},
+	                   {"abc123@gmail.a", false},
+	                   {"abc123@.com", false},
+	                   {"abc123@.com.com", false},
+	                   {".abc@abc.com", false},
+	                   {"abc()*@gmail.com", false},
+	                   {"abc@%*.com", false},
+	                   {"abc..2002@gmail.com", false},
+	                   {"abc.@gmail.com", false},
+	                   {"abc@abc@gmail.com", false},
+	                   {"abc@gmail.com.1a", false},
+	                   {"abc@gmail.com.aa.au", false}
+	           });
+	       }
+
+	       @Test
+	       public void givenEmailAsVar_ShouldReturnTrueorFalse() {
+	    	   System.out.println(expectedResult);
+	    	   System.out.println("Actual "+ validateEmail.validateEmail(emailTest));
+	    	   System.out.println(emailTest);
+	           assertEquals(expectedResult, validateEmail.validateEmail(emailTest));
+	       }
 }
